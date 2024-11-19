37b725989e81693486efa4f8e992638e63e1d336
Tarun7777777 <113977258+Tarun7777777@users.noreply.github.com>
2024-11-19 12:39:24 +0530

    Update DockerCommand.sh

--- DockerCommand.sh
+++ DockerCommand.sh
@@ -1,4 +1,4 @@
 # Before you run this code, please make sure your custom network is linked to the Postgres network. 
-+ If not, create a new network, attach it to the Postgres database. and then, run the code below.
+# If not, create a new network, attach it to the Postgres database. and then, run the code below.
 
 docker run -it --name data-copier-1 --network data-copier-nw --rm -v C:\Users\LENOVO\Research_mini_project\data\retail_db_json:/retail_db_json -e BASE_DIR=/retail_db_json -e DB_HOST=331a4ec67770 -e DB_PORT=5432 -e DB_NAME=retail_db -e DB_USER=retail_user -e DB_PASS=itversity data-copier python /data-copier/app_code_files/app.py departments,categories
